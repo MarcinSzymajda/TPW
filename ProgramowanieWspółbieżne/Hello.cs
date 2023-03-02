f6875fcc80f4032e04d3eee84254d347f3db0575
242546 <242546@edu.p.lodz.pl>
2023-03-02 19:01:43 +0100

    Hello world i dodanie testu

---
+++ ProgramowanieWspółbieżne/Hello.cs
@@ -0,0 +1,14 @@
+﻿using HelloClassNS;
+
+
+namespace mainNS
+{
+    public class Hello
+    {
+        static void Main(string[] args)
+        {
+            HelloClass temp = new HelloClass();
+            temp.consoleWrite("Hello");
+        }
+    }
+};
---
+++ ProgramowanieWspółbieżne/Tests/MainTest.cs
@@ -0,0 +1,17 @@
+﻿using HelloClassNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mainTestNS {
+
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void consoleWrite()
+        {
+            HelloClass temp = new HelloClass();
+            Assert.AreEqual(temp.consoleWrite("Hello"), "Hello");
+        }
+}
+
+}
