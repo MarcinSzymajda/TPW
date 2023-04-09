644a782266aeb0f58e3d5fb59d80a60a57d87512
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-04-09 19:59:51 +0200

    Etap 1 start

---
+++ ProgramowanieWspółbieżne/Logic/Ball.cs
@@ -0,0 +1,19 @@
+﻿namespace Logic
+{
+    internal class Ball
+    {
+        public int Radius { get; set; }
+        public int Y { get; set; }
+        public int X { get; set; }
+
+
+        public Ball(int radius,int x,int y)
+        {
+            this.Radius = radius;
+            this.X = x;
+            this.Y = y;
+        }
+
+  
+    }
+}
