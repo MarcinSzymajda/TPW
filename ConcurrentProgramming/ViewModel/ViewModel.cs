4daf0f7887597c3f2d80a97d62a1cb92e3a73790
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-04-14 20:36:23 +0200

    Minor improvements 2

--- ConcurrentProgramming/ViewModel/ViewModel.cs
+++ ConcurrentProgramming/ViewModel/ViewModel.cs
@@ -1,22 +1,73 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using System.Windows.Input;
+using ModelNS;
+using LogicNS;
 
 namespace ViewModelNS
 {
-
-  
     public class ViewModel : INotifyPropertyChanged
     {
-   
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public int WindowHeight { get; }
+        public int WindowWidth { get; }
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        public ICommand StartCommand { get; set; }
+        public ICommand StopCommand { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Model model { get; set; }
+
+        private ObservableCollection<Ball> balls;
+        public ObservableCollection<Ball> Balls
+        {
+            get => balls;
+            set
+            {
+                balls = value;
+                OnPropertyChanged();
+            }
+        }
+        public ViewModel()
+        {
+            balls = new();
+            WindowHeight = 600;
+            WindowWidth = 720;
+            model = new Model(WindowWidth, WindowHeight);
+            StartCommand = new Commands(Start);
+            StopCommand = new Commands(Stop);
+        }
+        
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             
         }
-
+        private async void Start()
+        {
+            Balls = model.GetStartingCirclePositions(Count);
+            while (model.IsAnimating)
+            {
+                await Task.Delay(10);
+                Balls = model.MoveCircle(balls);
+            }
+        }
+        private void Stop()
+        {
+            model.IsAnimating = false;
+        }
     }
-    }
+}
 
